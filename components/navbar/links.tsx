852533216a9412012d47383289ddba0ef8cc436d
MatusTabi <matustabi44@gmail.com>
2026-05-15 17:13:13 +0200

    fix: update Tournament link href to correct path

--- components/navbar/links.tsx
+++ components/navbar/links.tsx
@@ -8,7 +8,7 @@ import { usePathname } from 'next/navigation';
 export const links = [
     { name: 'Dashboard', href: '/dashboard', icon: LayoutDashboard },
     { name: 'Leaderboard', href: '#', icon: Trophy },
-    { name: 'Tournament', href: '#', icon: Award },
+    { name: 'Tournament', href: '/tournament', icon: Award },
     { name: 'Rules', href: '#', icon: Gavel },
 ];
 
