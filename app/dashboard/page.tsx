0a4674c57af25fc9b1361a292012ec75855da918
MatusTabi <matustabi44@gmail.com>
2026-05-15 16:14:37 +0200

    fix: adjust layout and spacing in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,16 @@
 import GlobalLeaders from '@/components/card/global-leaders';
+import Matches from '@/components/card/matches/matches';
 import MyPredictions from '@/components/card/my-predictions';
 import NextPuckDrop from '@/components/card/next-puck-drop';
 import Sidebar from '@/components/sidebar/sidebar';
 
 const DashboardPage = () => {
     return (
-        <div className="flex p-4 gap-16">
+        <div className="flex p-4 gap-16 justify-center">
             <Sidebar />
-            <div className="flex flex-col w-2/5">
+            <div className="flex flex-col w-2/5 gap-4">
                 <NextPuckDrop />
+                <Matches />
             </div>
             <div className="flex flex-col w-1/4 gap-4">
                 <MyPredictions />
