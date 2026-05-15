b6cbefb28213146f054badeb44ff61ace9d18412
MatusTabi <matustabi44@gmail.com>
2026-05-15 14:28:55 +0200

    fix: correct import and usage of Links component in Navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import Actions from './links';
+import Links from './links';
 import Logo from './logo';
 import ModeToggle from './mode-toggle';
 
@@ -6,7 +6,7 @@ const Navbar = () => (
     <header className="sticky top-0 z-50 w-full backdrop-blur-xl">
         <nav className="container mx-auto flex h-16 items-center justify-between px-4">
             <Logo />
-            <Actions />
+            <Links />
             <ModeToggle />
         </nav>
     </header>
