0fb985f8fa5d582655ca21e0f4dfbcd9d6d267b3
MatusTabi <matustabi44@gmail.com>
2026-05-15 22:46:40 +0200

    feat: enhance match card component to display team names and scores

--- components/card/matches/match.tsx
+++ components/card/matches/match.tsx
@@ -5,11 +5,26 @@ import { LiveMatchTag, OpenMatchTag, PredictedMatchTag } from './match-tag';
 import { cn } from '@/lib/utils';
 
 type MatchCardProps = {
+    homeTeam: string;
+    awayTeam: string;
+
+    homeScore: string | null;
+    awayScore: string | null;
+
     isLive: boolean;
     predicted: boolean;
+    hasEnded?: boolean;
 };
 
-const MatchCard = ({ isLive, predicted }: MatchCardProps) => (
+const MatchCard = ({
+    homeTeam,
+    awayTeam,
+    homeScore,
+    awayScore,
+    isLive,
+    predicted,
+    hasEnded,
+}: MatchCardProps) => (
     <div className="bg-primary-container border-inverse-on-surface border rounded-lg p-4 flex flex-col flex-1 relative">
         <div className="absolute top-0 right-0 flex items-center">
             {isLive ? (
@@ -28,29 +43,39 @@ const MatchCard = ({ isLive, predicted }: MatchCardProps) => (
             )}
         </div>
         <h1 className="font-semibold text-lg">Group A</h1>
-        <div className="flex gap-4 items-center justify-center my-4">
-            <span>Canada</span>
-            <div className="flex gap-2 items-center">
+        <div className="grid grid-cols-[1fr_auto_1fr] items-center my-4 w-full">
+            <span className="justify-self-end text-right">
+                {homeTeam.replace('Ice Hockey', '')}
+            </span>
+
+            <div className="flex gap-2 items-center justify-center h-12">
                 {isLive || predicted ? (
-                    <>
-                        <span className="text-4xl font-bold">2</span>-
-                        <span className="text-4xl font-bold">1</span>
-                    </>
+                    <div className="h-12 mx-2 flex items-center">
+                        <span className="text-4xl font-bold">
+                            {homeScore ?? '0'}
+                        </span>
+                        <span className="mx-2">-</span>
+                        <span className="text-4xl font-bold">
+                            {awayScore ?? '0'}
+                        </span>
+                    </div>
                 ) : (
-                    <>
+                    <div className="mx-2 flex items-center">
                         <input
-                            className="bg-surface-container-lowest border-outline w-10 h-14 p-2 text-4xl font-bold text-center"
+                            className="bg-surface-container-lowest border-outline w-10 h-12 p-2 text-4xl font-bold text-center"
                             placeholder="0"
                         />
-                        -
+                        <span className="mx-2">-</span>
                         <input
-                            className="bg-surface-container-lowest border-outline w-10 h-14 p-2 text-4xl font-bold text-center"
+                            className="bg-surface-container-lowest border-outline w-10 h-12 p-2 text-4xl font-bold text-center"
                             placeholder="0"
                         />
-                    </>
+                    </div>
                 )}
             </div>
-            <span>Sweden</span>
+            <span className="justify-self-start text-left">
+                {awayTeam.replace(/Ice Hockey/i, '')}
+            </span>
         </div>
 
         <Button
