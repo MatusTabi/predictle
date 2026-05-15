84dbe010192ba6161c5d90dfde21fcc62124901e
MatusTabi <matustabi44@gmail.com>
2026-05-15 19:41:51 +0200

    feat: add placeholder tournament table demo

---
+++ components/table/tournament-table.tsx
@@ -0,0 +1,7 @@
+const TournamentTable = () => (
+    <div className="bg-primary-container border border-inverse-on-surface rounded-lg p-4 flex justify-center items-center">
+        <h1>Tournament Standings - TBA</h1>
+    </div>
+);
+
+export default TournamentTable;
