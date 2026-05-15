0f418bd6a9e1d635eb97c4102b0f68808ed557a9
MatusTabi <matustabi44@gmail.com>
2026-05-15 22:46:14 +0200

    feat: create tournament content component for match display and filtering

--- app/(content)/tournament/page.tsx
+++ app/(content)/tournament/page.tsx
@@ -1,35 +1,10 @@
 import { getMatches } from '@/backend/matches/action';
-import MatchCard from '@/components/card/matches/match';
-import DateNavigator from '@/components/filter/date-navigator';
-import TournamentTable from '@/components/table/tournament-table';
+import TournamentContent from '@/components/tournament/content';
 
 const TournamentPage = async () => {
     const matches = await getMatches();
 
-    return (
-        <>
-            <div className="flex flex-col w-3/5 gap-4">
-                <h1 className="text-4xl font-bold">
-                    Tournament & Prediction Page
-                </h1>
-                <div className="flex justify-between">
-                    <span className="text-lg">World Hockey Championship</span>
-                    <DateNavigator />
-                </div>
-                <section className="grid-cols-2 grid gap-4">
-                    {matches.map((match) => (
-                        <MatchCard
-                            isLive={match.isLive}
-                            predicted={!!match.predicted}
-                        />
-                    ))}
-                </section>
-            </div>
-            <div className="flex flex-col w-1/4 gap-4">
-                <TournamentTable />
-            </div>
-        </>
-    );
+    return <TournamentContent matches={matches} />;
 };
 
 export default TournamentPage;
---
+++ components/tournament/content.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useState } from 'react';
+import DateNavigator from '../filter/date-navigator';
+import { MatchDTO } from '@/backend/matches/types';
+import MatchCard from '../card/matches/match';
+import TournamentTable from '../table/tournament-table';
+
+type TournamentContentProps = {
+    matches: MatchDTO[];
+};
+
+const TournamentContent = ({ matches }: TournamentContentProps) => {
+    const [date, setDate] = useState<Date>(new Date());
+
+    console.log('Received matches:', matches);
+
+    const filteredMatchesByDate = matches.filter((match) => {
+        const matchDate = new Date(match.date);
+
+        return (
+            matchDate.getDate() === date.getDate() &&
+            matchDate.getMonth() === date.getMonth() &&
+            matchDate.getFullYear() === date.getFullYear()
+        );
+    });
+
+    return (
+        <>
+            <div className="flex flex-col w-3/5 gap-4">
+                <h1 className="text-4xl font-bold">
+                    Tournament & Prediction Page
+                </h1>
+                <div className="flex justify-between">
+                    <span className="text-lg">World Hockey Championship</span>
+                    <DateNavigator value={date} onChange={setDate} />
+                </div>
+                <section className="grid-cols-2 grid gap-4">
+                    {filteredMatchesByDate.map((match) => (
+                        <MatchCard
+                            key={`${match.homeTeam}-${match.awayTeam}-${match.date}`}
+                            homeTeam={match.homeTeam}
+                            awayTeam={match.awayTeam}
+                            homeScore={match.homeScore}
+                            awayScore={match.awayScore}
+                            isLive={match.isLive}
+                            predicted={!!match.predicted}
+                        />
+                    ))}
+                </section>
+            </div>
+            <div className="flex flex-col w-1/4 gap-4">
+                <TournamentTable />
+            </div>
+        </>
+    );
+};
+
+export default TournamentContent;
