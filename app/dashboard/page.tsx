58e88a3c99d86e323cf3e34c0de1b9ffc22d0697
MatusTabi <matustabi44@gmail.com>
2026-05-15 14:41:53 +0200

    feat: update dashboard page with additional cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,19 @@
+import GlobalLeaders from '@/components/card/global-leaders';
+import MyPredictions from '@/components/card/my-predictions';
+import NextPuckDrop from '@/components/card/next-puck-drop';
 import Sidebar from '@/components/sidebar/sidebar';
 
 const DashboardPage = () => {
     return (
-        <div className="flex p-4">
+        <div className="flex p-4 gap-16">
             <Sidebar />
+            <div className="flex flex-col w-2/5">
+                <NextPuckDrop />
+            </div>
+            <div className="flex flex-col w-1/4 gap-4">
+                <MyPredictions />
+                <GlobalLeaders />
+            </div>
         </div>
     );
 };
