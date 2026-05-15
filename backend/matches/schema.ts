80bf97e38e3ba359761818a8367086b07fce6b87
MatusTabi <matustabi44@gmail.com>
2026-05-15 20:56:59 +0200

    feat: add MatchSchema and MatchSchemaResponse for match data validation

---
+++ backend/matches/schema.ts
@@ -0,0 +1,18 @@
+import z from 'zod';
+
+export const MatchSchema = z.object({
+    strHomeTeam: z.string().max(100),
+    strAwayTeam: z.string().max(100),
+    intHomeScore: z.string().nullable(),
+    intAwayScore: z.string().nullable(),
+    strTimeLocal: z.string(),
+    dateEventLocal: z.string(),
+});
+
+export const MatchSchemaResponse = z.object({
+    events: z.array(MatchSchema),
+});
+
+export type MatchSchema = z.infer<typeof MatchSchema>;
+
+export type MatchType = z.infer<typeof MatchSchemaResponse>;
