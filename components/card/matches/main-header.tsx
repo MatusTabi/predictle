50b9fab7200e84315665481e0e1b7ba8350db7ae
MatusTabi <matustabi44@gmail.com>
2026-05-15 19:41:28 +0200

    feat: update header title to reflect today's matches

--- components/card/matches/main-header.tsx
+++ components/card/matches/main-header.tsx
@@ -1,6 +1,6 @@
 const MainHeader = () => (
     <div className="mb-4 flex justify-between p-2">
-        <h1 className="font-semibold text-2xl">Upcoming Matches</h1>
+        <h1 className="font-semibold text-2xl">Today's Matches</h1>
     </div>
 );
 
