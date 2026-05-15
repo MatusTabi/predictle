5f348e0a44971f369f52bf45d266fa5c9e2ecf6e
MatusTabi <matustabi44@gmail.com>
2026-05-15 20:56:32 +0200

    feat: convert tournament page to async and fetch matches dynamically

--- app/(content)/tournament/page.tsx
+++ app/(content)/tournament/page.tsx
@@ -1,26 +1,35 @@
+import { getMatches } from '@/backend/matches/action';
 import MatchCard from '@/components/card/matches/match';
 import DateNavigator from '@/components/filter/date-navigator';
 import TournamentTable from '@/components/table/tournament-table';
 
-const TournamentPage = () => (
-    <>
-        <div className="flex flex-col w-3/5 gap-4">
-            <h1 className="text-4xl font-bold">Tournament & Prediction Page</h1>
-            <div className="flex justify-between">
-                <span className="text-lg">World Hockey Championship</span>
-                <DateNavigator />
+const TournamentPage = async () => {
+    const matches = await getMatches();
+
+    return (
+        <>
+            <div className="flex flex-col w-3/5 gap-4">
+                <h1 className="text-4xl font-bold">
+                    Tournament & Prediction Page
+                </h1>
+                <div className="flex justify-between">
+                    <span className="text-lg">World Hockey Championship</span>
+                    <DateNavigator />
+                </div>
+                <section className="grid-cols-2 grid gap-4">
+                    {matches.map((match) => (
+                        <MatchCard
+                            isLive={match.isLive}
+                            predicted={!!match.predicted}
+                        />
+                    ))}
+                </section>
             </div>
-            <section className="grid-cols-2 grid gap-4">
-                <MatchCard isLive={true} predicted={true} />
-                <MatchCard isLive={true} predicted={true} />
-                <MatchCard isLive={false} predicted={true} />
-                <MatchCard isLive={false} predicted={false} />
-            </section>
-        </div>
-        <div className="flex flex-col w-1/4 gap-4">
-            <TournamentTable />
-        </div>
-    </>
-);
+            <div className="flex flex-col w-1/4 gap-4">
+                <TournamentTable />
+            </div>
+        </>
+    );
+};
 
 export default TournamentPage;
