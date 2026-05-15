6ada05b8fe8754d080d2349ce330b8d2011024e0
MatusTabi <matustabi44@gmail.com>
2026-05-15 14:29:01 +0200

    feat: implement sidebar navigation with dynamic links and active state

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,9 +1,37 @@
+'use client';
+
 import Rank from './rank';
+import { links } from '../navbar/links';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = () => {
+    const pathName = usePathname();
+
     return (
         <section className="w-1/4 bg-primary-container rounded-lg p-4 flex flex-col gap-4">
             <Rank />
+            <nav className="flex flex-col gap-2">
+                {links.map(({ name, href, icon: Icon }) => {
+                    const isActive = pathName.startsWith(href);
+
+                    return (
+                        <Link
+                            key={name}
+                            href={href}
+                            className={cn(
+                                'flex items-center gap-x-2 rounded-lg px-3 py-2 font-medium text-lg text-foreground/70 hover:text-foreground transition-colors',
+                                isActive &&
+                                    'bg-surface-container-high text-primary-foreground',
+                            )}
+                        >
+                            <Icon className="h-5 w-5 inline" />
+                            {name}
+                        </Link>
+                    );
+                })}
+            </nav>
         </section>
     );
 };
