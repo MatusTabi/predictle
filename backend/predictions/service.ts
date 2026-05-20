3a7b215265a5850ce5b5a57588308d2e8d227b9b
MatusTabi <matustabi44@gmail.com>
2026-05-20 18:58:01 +0200

    feat: implement user validation and tournament participation check in prediction submission

--- backend/predictions/service.ts
+++ backend/predictions/service.ts
@@ -1,6 +1,25 @@
+import { ensureTournamentParticipation } from '../leaderboard/repository';
+import { getUsernameById } from '../user/repository';
 import { createPrediction } from './repository';
 import { PredictionRequestDTO } from './types';
 
 export const submitPrediction = async (prediction: PredictionRequestDTO) => {
+    const userName = await getUsernameById(prediction.userId);
+
+    if (!userName) {
+        throw new Error('User not found');
+    }
+
+    const result = await ensureTournamentParticipation(
+        prediction.userId,
+        userName,
+    );
+
+    console.log('ensureTournamentParticipation result: ', result);
+
+    if (result.rows.length === 0) {
+        throw new Error('User is not participating in the tournament');
+    }
+
     return await createPrediction(prediction);
 };
