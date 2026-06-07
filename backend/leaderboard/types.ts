ae3e75bf1bf9a26306ae35fe763016e1f93583df
Timotej Šimurka <simurka@ekom.sk>
2026-06-07 18:33:03 +0200

    fix: update DTO to match db update

--- backend/leaderboard/types.ts
+++ backend/leaderboard/types.ts
@@ -2,9 +2,7 @@ export type TournamentParticipantDTO = {
     id: string;
 
     userId: string;
-    userName: string;
 
-    rank: number;
     correctWinners: number | null;
     correctScores: number | null;
     totalPredictions: number | null;
