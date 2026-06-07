51b8716a8e08b9ff9a9df3318a06737bb8e136a3
Timotej Šimurka <simurka@ekom.sk>
2026-06-07 18:22:23 +0200

    feat: more detailed db design

--- db/schema/tournament-participant.ts
+++ db/schema/tournament-participant.ts
@@ -1,14 +1,18 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { users } from './auth';
+import { tournament } from './tournament';
 
 export const tournamentParticipant = sqliteTable('tournament_participant', {
     id: text('id')
         .primaryKey()
         .$defaultFn(() => crypto.randomUUID()),
-    userId: text('userId').notNull().unique(),
-    userName: text('userName').notNull(),
-    // tournamentId: text('tournamentId').notNull().unique(),
+    userId: text('userId').notNull().unique()
+                .notNull()
+                .references(() => users.id, { onDelete: 'cascade' }),
+    tournamentId: text('tournamentId').notNull().unique()
+                .notNull()
+                .references(() => tournament.id, { onDelete: 'cascade' }),
 
-    rank: integer('rank').notNull(),
     correctWinners: integer('correctWinners').default(0),
     correctScores: integer('correctScores').default(0),
     totalPredictions: integer('totalPredictions').default(0),
