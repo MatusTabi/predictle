6e6795d58293a3797e3f0d4b2359ecc4a626a524
MatusTabi <matustabi44@gmail.com>
2026-06-07 17:15:46 +0200

    chore: comment out authorization header check in GET request

--- app/api/internal/route.ts
+++ app/api/internal/route.ts
@@ -2,7 +2,7 @@ import { syncMatchesFromSportsDb } from '@/backend/matches/service';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-    const authHeader = request.headers.get('authorization');
+    // const authHeader = request.headers.get('authorization');
 
     // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     //     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
